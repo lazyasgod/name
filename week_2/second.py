dcf20dcba6102f7ec8888953ab219e9865e5a138
lazyasgod <4709064@gmail.com>
2014-07-15 19:16:20 +0400

    Update second.py

--- week_2/second.py
+++ week_2/second.py
@@ -1,13 +1,22 @@
-def max_prime():
-   min_prime = 3                 
-   usrnum = int(input("put number >"))     
-   x = []
-   while min_prime <= usrnum:
-      if usrnum % min_prime == 0:
-         x.append(min_prime)    
-         usrnum = usrnum/min_prime
-      else:
-         min_prime = min_prime + 2     
-   print ("max prime =", max(x))
+def prime(n):
+  n = abs(int(n))
+  if n < 2 :
+     return False
+  if n == 2:
+     return True
+  if not n & 1:
+     return False 
+  for x in range(3, int(n**0.5)+1, 2):
+     if n % x == 0:
+        return False
+  return True
 
-max_prime()
+
+def max_prime(usrnum):
+    if prime(usrnum) == True:
+        return usernum
+    if usrnum >= 2:
+        for i in range(2, int(usrnum)): 
+            if prime(i) == True and not usrnum % i == 0 :
+                x = i 
+        return x
